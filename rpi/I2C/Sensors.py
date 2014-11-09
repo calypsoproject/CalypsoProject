4e771c560f993503622eaff243a5b2f8dfe8b1b9
prog <klopcic.vid@gmail.com>
2014-11-09 10:19:55 +0100

    fixed ADXL345

--- rpi/I2C/Sensors.py
+++ rpi/I2C/Sensors.py
@@ -1,11 +1,11 @@
-from accelerometer import adxl345
+from accelerometer import ADXL345
 from gyroscope import SensorITG3200
 from compass import hmc5883l
 
 
 class Sensors:
     def __init__(self, i2c_common, accelerometer_addr=0x00, gyroscope_addr=0x00, compass_addr=0x00):
-        self.accelerometer = adxl345(i2c_common, address=accelerometer_addr)
+        self.accelerometer = ADXL345(i2c_common, address=accelerometer_addr)
         self.gyroscope = SensorITG3200(i2c_common, address=gyroscope_addr)
         self.compass = hmc5883l(i2c_common, address=compass_addr)
 
